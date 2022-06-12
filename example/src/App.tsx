2514d3c2a549725a0d25e6859b801b46b57b431a
Vitaliy Zakutniy <stalker-52@mail.ru>
2022-06-12 14:48:30 +0300

    Rename root component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'selectable-grid'
+import { SelectableGrid } from 'selectable-grid'
 import 'selectable-grid/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <SelectableGrid text='Create React Library Example 😄' />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,6 @@ interface Props {
   text: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
+export const SelectableGrid = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
