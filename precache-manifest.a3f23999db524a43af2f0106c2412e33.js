d427c0ab9a08f6b6910e18bbac3f327f77e204b0
Vitaliy Zakutniy <stalker-52@mail.ru>
2022-07-23 22:10:16 +0300

    Updates

---
+++ precache-manifest.a3f23999db524a43af2f0106c2412e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e793f0001bfe957dc9d51ea4855918",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18794f198914c9975c75",
+    "url": "./static/css/main.6f4a8042.chunk.css"
+  },
+  {
+    "revision": "6e0a1dcba1bbea9c3fcc",
+    "url": "./static/js/2.1f054447.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1f054447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18794f198914c9975c75",
+    "url": "./static/js/main.776c3e59.chunk.js"
+  },
+  {
+    "revision": "a8f6209adcdf5a86f698",
+    "url": "./static/js/runtime-main.6d3f4d57.js"
+  }
+]);
