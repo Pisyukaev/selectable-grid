a5e6c2dd8987bbd705dd9734a334aa05289006ab
Vitaly Zakutny <stalker-52@mail.ru>
2022-10-09 16:47:27 +0300

    fix: set position canvas on left-top corner

--- packages/selectable-grid/src/selectable-grid.ts
+++ packages/selectable-grid/src/selectable-grid.ts
@@ -56,8 +56,8 @@ export class SelectableGrid {
 
   #setCanvasStyles() {
     this.#canvas.style.position = 'absolute'
-    // this.#canvas.style.top = '0'
-    // this.#canvas.style.left = '0'
+    this.#canvas.style.top = '0'
+    this.#canvas.style.left = '0'
   }
 
   #subscribe() {
