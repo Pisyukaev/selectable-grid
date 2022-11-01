6ce6be799cdf8dd11f31c79128cdd85bdd7bef80
Vitaly Zakutny <stalker-52@mail.ru>
2022-11-01 21:20:57 +0300

    chore: added debounce ane throttle functions

---
+++ packages/selectable-grid/src/utils.ts
@@ -0,0 +1,32 @@
+export function debounce<T extends (...args: any[]) => void>(
+  fn: T,
+  ms: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>
+
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    timeoutId = setTimeout(() => fn(args), ms)
+  }
+}
+
+export function throttle<T extends (...args: any[]) => ReturnType<T>>(
+  fn: T,
+  ms: number
+): (...args: Parameters<T>) => ReturnType<T> {
+  let isThrottle: boolean
+  let lastResult: ReturnType<T>
+
+  return (...args) => {
+    if (!isThrottle) {
+      isThrottle = true
+      setTimeout(() => (isThrottle = false), ms)
+      lastResult = fn(args)
+    }
+
+    return lastResult
+  }
+}
