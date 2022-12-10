6f25ea0b10761e2fa3fc2c63fa79fcbc73ed120c
Vitaly Zakutny <stalker-52@mail.ru>
2022-12-10 16:37:26 +0300

    feat: added example for use Vue selectable-grid component

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import SelectableGrid from "@selectable-grid/vue";
+
+const imgRef = ref<HTMLImageElement>();
+</script>
+
+<template>
+  <div class="image-container">
+    <img
+      ref="imgRef"
+      id="img"
+      src="https://avatars.mds.yandex.net/get-mpic/5219318/img_id3258925543469117996.jpeg/orig"
+      alt=""
+    />
+  </div>
+  <SelectableGrid
+    v-if="imgRef"
+    :image-container="imgRef"
+    :cell-count="15"
+    :options="{
+      imageContainer: imgRef,
+      cellCount: 15,
+    }"
+  />
+</template>
+
+<style scoped>
+#img {
+  height: 500px;
+}
+</style>
