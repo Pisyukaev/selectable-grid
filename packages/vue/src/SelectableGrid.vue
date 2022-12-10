5a329a4fa3c8fbda6fbe7afa7ee1b03fc3647898
Vitaly Zakutny <stalker-52@mail.ru>
2022-12-10 16:36:18 +0300

    feat: Added selectable-grid package for Vue

---
+++ packages/vue/src/SelectableGrid.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted, watch } from 'vue'
+import SelectableGrid from 'selectable-grid'
+import type { Options } from 'selectable-grid'
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  options: {
+    type: Object as PropType<Options>,
+    required: true
+  }
+})
+
+const selectableGrid = ref<SelectableGrid>()
+
+onMounted(() => {
+  selectableGrid.value = new SelectableGrid(props.options)
+})
+
+watch(props, () => selectableGrid.value?.setOptions(props.options))
+</script>
---
+++ packages/vue/src/index.ts
@@ -0,0 +1,5 @@
+import SelectableGrid from './SelectableGrid.vue'
+import type { Options, Area, FillStrokeStyles, Point } from 'selectable-grid'
+
+export default SelectableGrid
+export type { Options, Area, FillStrokeStyles, Point }
