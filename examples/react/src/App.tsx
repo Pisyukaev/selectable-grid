c33cb6494e86836de3425c440103957362cdb765
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-01-14 04:41:12 +0800

    upd rageywide

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,17 +1,16 @@
 import React, { useRef, useState } from 'react'
 import SelectableGrid from '@selectable-grid/react'
 
-import spanchBobSrc from './img/spanch-bob.webp'
-
 const App = () => {
   const imgRef = useRef<HTMLImageElement>()
   const [_, setIsLoad] = useState(false)
+
   return (
     <div style={{ position: 'relative' }}>
       <img
         ref={imgRef}
-        src={spanchBobSrc}
-        alt=''
+        src='spanch-bob.webp'
+        alt='Spanch Bob'
         height={500}
         onLoad={() => setIsLoad(true)}
       />
