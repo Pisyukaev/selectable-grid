83b82f5242cc80b4192ca0297bf09ce57a7a8541
Vitaly Zakutny <stalker-52@mail.ru>
2023-01-14 00:50:50 +0300

    chore: upd react example

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,20 +1,18 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import SelectableGrid from '@selectable-grid/react'
 
 const App = () => {
-  const imgRef = useRef<HTMLImageElement>()
-  const [_, setIsLoad] = useState(false)
-
   return (
     <div style={{ position: 'relative' }}>
-      <img
-        ref={imgRef}
-        src='spanch-bob.webp'
-        alt='Spanch Bob'
-        height={500}
-        onLoad={() => setIsLoad(true)}
+      <SelectableGrid
+        cellCount={20}
+        type='img'
+        containerProps={{
+          src: 'spanch-bob.webp',
+          alt: 'Spanch Bob',
+          height: 500
+        }}
       />
-      <SelectableGrid cellCount={20} imageContainer={imgRef.current} />
     </div>
   )
 }
